8ea3724fc1de99e55c6d96b814a13de6707d9766
ishwar1510 <ijh.hkr9@gmail>
2020-12-29 07:51:06 +0530

    js division

---
+++ divisible/script.js
@@ -0,0 +1,8 @@
+var xarray = [2,3,4,6,9,12];
+
+for (let i = 0; i <= xarray.length - 1; i++) {
+     if (xarray[i] % 3 == 0) {
+       console.log(xarray[i], "is divided by 3");
+     } else console.log(xarray[i],"is Not divided by 3");
+    }
+   
