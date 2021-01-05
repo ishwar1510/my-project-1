284548cf89698d40ac95ba9f3187794ca4c7edd4
ishwar1510 <ijh.hkr9@gmail>
2021-01-05 20:04:06 +0530

    division of 7

--- divisible/script.js
+++ divisible/script.js
@@ -1,8 +1,15 @@
-var xarray = [2,3,4,6,9,12];
+/*var xarray = [2,3,4,6,9,12];
 
 for (let i = 0; i <= xarray.length - 1; i++) {
      if (xarray[i] % 3 == 0) {
        console.log(xarray[i], "is divided by 3");
      } else console.log(xarray[i],"is Not divided by 3");
     }
-   
+   */
+  
+var x = [7,14,23,62,28,32,30]
+for (let i = 0; i <=x.length -1; i++) {
+     if (x[i] % 7 == 0) {
+       console.log(x[i], "is divided by 7");
+     } else console.log(x[i],"is Not divided by 7");
+    }
