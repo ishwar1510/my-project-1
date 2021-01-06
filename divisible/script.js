b4768413d3ef62f2fd99ec9ab495b194030bd063
ishwar1510 <ijh.hkr9@gmail>
2021-01-06 20:37:47 +0530

    positive or negetive

--- divisible/script.js
+++ divisible/script.js
@@ -1,4 +1,7 @@
-/*var xarray = [2,3,4,6,9,12];
+ 
+  /*-----------1st method------*/
+  
+  /*var xarray = [2,3,4,6,9,12];
 
 for (let i = 0; i <= xarray.length - 1; i++) {
      if (xarray[i] % 3 == 0) {
@@ -7,9 +10,13 @@ for (let i = 0; i <= xarray.length - 1; i++) {
     }
    */
   
-var x = [7,14,23,62,28,32,30]
+
+   /*------------2nd method-------*/
+ var x = [7,14,23,62,28,32,30]
 for (let i = 0; i <=x.length -1; i++) {
      if (x[i] % 7 == 0) {
        console.log(x[i], "is divided by 7");
-     } else console.log(x[i],"is Not divided by 7");
+     } 
+     else console.log(x[i],"is Not divided by 7");
     }
+    
